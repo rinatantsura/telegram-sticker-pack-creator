5a8667d72a157551c698aa947cc25a85d1a1a733
irinatantsura <irina_tantsura@mail.ru>
2025-08-14 21:58:09 +0200

    added receiving from the user of a picture and local saving

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"fmt"
 	"github.com/go-telegram/bot"
 	"github.com/go-telegram/bot/models"
+	"io"
+	"net/http"
 	"os"
 	"os/signal"
 )
@@ -45,11 +47,46 @@ func main() {
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
-	_, err := b.SendMessage(ctx, &bot.SendMessageParams{
-		ChatID: update.Message.Chat.ID,
-		Text:   "I can send messages",
+
+	if update.Message == nil || len(update.Message.Photo) == 0 {
+		_, err := b.SendMessage(ctx, &bot.SendMessageParams{
+			ChatID: update.Message.Chat.ID,
+			Text:   "Please, send me a photo",
+		})
+		if err != nil {
+			fmt.Println("Error sending message")
+		}
+		return
+	}
+	file, err := b.GetFile(ctx, &bot.GetFileParams{
+		FileID: update.Message.Photo[len(update.Message.Photo)-1].FileID,
 	})
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error getting file info")
 	}
+
+	url := fmt.Sprintf("https://api.telegram.org/file/bot%s/%s", b.Token(), file.FilePath)
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error downloading file")
+		return
+	}
+	defer resp.Body.Close()
+
+	filename := fmt.Sprintf("photo_%d.jpg", update.Message.Date)
+	out, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Error creating file")
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		fmt.Println("Error copying file")
+		return
+	}
+
+	fmt.Println("Saved photo", filename)
 }
